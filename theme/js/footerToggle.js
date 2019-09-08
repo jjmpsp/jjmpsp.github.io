d7469d4c211861e1abc36ef80d97c600b93a6875
Joel Murphy <joel.murphy@wales.nhs.uk>
2019-09-08 16:33:05 +0100

    Removed clicktoggle as its now an unused dependency

--- theme/js/footerToggle.js
+++ theme/js/footerToggle.js
@@ -1,71 +1,98 @@
+let footerDocked = null;
+let tocActive = null;
+
 window.setCookie = function (key, value) {
     var d = new Date();
     var expiresInDays = 30 * 24 * 60 * 60 * 1000;
     d.setTime(d.getTime() + expiresInDays);
     var expires = "expires=" + d.toGMTString();
     document.cookie = key + '=' + value + "; " + expires + ";path=/";
+    return value;
 };
 
 window.getCookie = function (key) {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-        var c = cookies[i].trim();
-        if (c.indexOf(key) === 0) {
-            return c[key];
+    var name = key + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
         }
     }
+    return "";
 };
 
 function showTOC() {
     $("#float-toc").show();
     setCookie("TOC_ACTIVE", true);
+    tocActive = true;
 }
 
 function hideTOC() {
     $("#float-toc").hide();
     setCookie("TOC_ACTIVE", false);
+    tocActive = false;
 }
 
 function dockStickyFooter() {
     $("#footer").show().css("position", "fixed");
     setCookie("FOOTER_DOCKED", true);
+    footerDocked = true;
 }
 
 function undockStickyFooter() {
     $("#footer").show().css("position", "relative");
     setCookie("FOOTER_DOCKED", false);
+    footerDocked = false;
 }
 
 $(document).ready(function () {
-    $("#toggleStickyFooter").clickToggle(
-        function () {
-            $(this).text("⬇");
-            dockStickyFooter();
-        },
-        function () {
-            $(this).text("⬆");
-            undockStickyFooter();
-        }
-    );
-
-    $("#toggleTableOfContents").clickToggle(
-        function () {
-            $(this).text("⬇");
-            showTOC(this);
-        },
-        function () {
-            $(this).text("⬆");
-            hideTOC();
-        }
-    );
-});
-
-
-$(document).ready(function () {
-    //hideTOC();
-    undockStickyFooter();
-
-
     // Get docked settings from cookies
+    const _footerDocked = getCookie("FOOTER_DOCKED");
+    const _tocActive = getCookie("TOC_ACTIVE");
 
+    if (_footerDocked === 'true') {
+        footerDocked = true;
+        $("#toggleStickyFooter").text("⬇");
+        dockStickyFooter();
+    } else {
+        footerDocked = false;
+        $("#toggleStickyFooter").text("⬆");
+        undockStickyFooter();
+    }
+
+    if (_tocActive === 'true') {
+        tocActive = true;
+        $("#toggleTableOfContents").text("⬇");
+        showTOC();
+    } else {
+        tocActive = false;
+        $("#toggleTableOfContents").text("⬆");
+        hideTOC();
+    }
+
+    $("#toggleStickyFooter").click(function () {
+        console.log('stickyfooter='+footerDocked);
+        if (footerDocked) {
+            undockStickyFooter();
+            $(this).text("⬆");
+        } else {
+            dockStickyFooter();
+            $(this).text("⬇");
+        }
+    });
+
+    $("#toggleTableOfContents").click(function () {
+        console.log('tocactive='+tocActive);
+        if (tocActive) {
+            hideTOC();
+            $(this).text("⬆");
+        } else {
+            showTOC();
+            $(this).text("⬇");
+        }
+    });
 });
