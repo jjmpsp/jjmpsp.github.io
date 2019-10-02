b945d50272c102b44e17a1d4336ab5d8e3927c5a
Joel Murphy <joel.murphy@wales.nhs.uk>
2019-10-02 15:41:45 +0100

    different configuration file for staging

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -13,18 +13,21 @@ set -e
 rm -rf _site
 mkdir _site
 
-# clone a remote repo to "_site"
+# clone a remote repo to "_site" and build it
 if [ $TRAVIS_REPO_SLUG == "jjmpsp/jjmpsp.github.io" ]; then
   git clone https://${GH_TOKEN}@github.com/jjmpsp/jjmpsp.github.io.git --branch master _site
+
+  # build with Jekyll into "_site" with production config
+  JEKYLL_ENV=production bundle exec jekyll build
 elif [ $TRAVIS_REPO_SLUG == "jjmpsp-staging/jjmpsp-staging.github.io" ]; then
   git clone https://${GH_TOKEN}@github.com/jjmpsp-staging/jjmpsp-staging.github.io.git --branch master _site
+
+  # build with Jekyll into "_site" with staging config
+  JEKYLL_ENV=production bundle exec jekyll build --config _config-staging.yml
 else
   echo "Invalid repository - see cibuild.sh source code for more info..."
   exit 0
 fi
-
-# build with Jekyll into "_site"
-JEKYLL_ENV=production bundle exec jekyll build
 
 # push
 cd _site
